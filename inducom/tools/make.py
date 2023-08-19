7841e59b33290cb1b97cb06cc32bd8eb81ab86cc
Rikus-C <rcoetzee967@gmail.com>
2023-08-19 14:02:32 +0200

    worked on python make file

--- inducom/tools/make.py
+++ inducom/tools/make.py
@@ -1,18 +1,12 @@
 # compile current c++ backend code and store exacutables in bin file
 import os           as os
-import subprocess   as sub
 import json         as json
 # g++ main.cpp sqlite3.o -Ipath\to\sqlite3
 os.system("clear")
 
-json_file = open('./settings.json', 'r')
-settings = json.load(json_file)
-json_file.close()
-
-directories = settings["c libraries to compile"]
-
-# first compile all the c code present
-
+json_file       = open('./settings.json', 'r')
+settings        = json.load(json_file)
+directories     = settings["c libraries to compile"]
 bin_folder      = "../bin/"
 exe_name        = "c_libraries"
 command         = "gcc -g -O -Wall "
@@ -26,10 +20,13 @@ for current_dir in directories :
             files.append(current_dir + current_file)
 
 if len(files) > 0 :
-    print("Compiled files:\n")
+    print("Compiled Files:\n")
     print("============================================================")
     for fl in files : print(fl)
     print("============================================================\n\n")
-    sub.run(command + "-o " + bin_folder + exe_name, shell=True)
+    os.system(command + "-o " + bin_folder + exe_name)
+
+json_file.close()
+
 
 
